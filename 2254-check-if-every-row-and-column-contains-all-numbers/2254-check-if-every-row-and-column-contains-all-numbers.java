f43f31f7d2aaf21d789b00415391c30a772b821d
mahesdav <35679809+mahesdav@users.noreply.github.com>
2023-10-29 16:47:52 +0530

    Time: 5 ms (89.03%), Space: 45 MB (9.05%) - LeetHub

--- 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.java
+++ 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean checkValid(int[][] matrix) {
 
-        // int length = matrix.length;
+        int length = matrix.length;
 
-        // Set<Integer> set = new HashSet();
+       // Set<Integer> set = new HashSet();
 
         // for(int i=0;i<length;i++){
         //     if(set.contains(matrix[0][i]))
@@ -11,23 +11,30 @@ class Solution {
         //     else
         //      set.add(matrix[0][i]);
         // }
-
-        // for(int i=1;i<length;i++){
-        //     Set<Integer> internalSet = new HashSet();
-        //     for(int j=0;j<matrix[i].length;j++){
-        //         if(internalSet.contains(matrix[i][j]) || !set.contains(matrix[i][j])){
-        //             return false;
-        //         }else{
-        //             internalSet.add(matrix[i][j]);
-        //         }
-        //     }
-        //     i=i;
-        // }
-        // return true;
-        Set< String > set = new HashSet<>();
-        for ( int i = 0; i < matrix.length; i++ )
-            for ( int j = 0; j < matrix.length; j++ )
-                if (!set.add(matrix[i][j]+"r"+i) || !set.add(matrix[i][j] +"c"+j)) return false;
+       
+        for(int i=0;i<length;i++){
+           // Set<Integer> internalSet = new HashSet();
+            boolean[] isRowVisited = new boolean[length+1];
+            boolean[] isColumnVisited = new boolean[length+1];
+            for(int j=0;j<matrix[i].length;j++){
+               // if(internalSet.contains(matrix[i][j]) || !set.contains(matrix[i][j])){
+                int rVal = matrix[i][j];
+                int cVal = matrix[j][i];
+                if(isRowVisited[rVal] || isColumnVisited[cVal]){
+                    return false;
+                }else{
+                    // internalSet.add(matrix[i][j]);
+                    isRowVisited[rVal] = true;
+                    isColumnVisited[cVal] = true;
+                }
+            }
+            i=i;
+        }
         return true;
+        // Set< String > set = new HashSet<>();
+        // for ( int i = 0; i < matrix.length; i++ )
+        //     for ( int j = 0; j < matrix.length; j++ )
+        //         if (!set.add(matrix[i][j]+"r"+i) || !set.add(matrix[i][j] +"c"+j)) return false;
+        // return true;
     }
 }
