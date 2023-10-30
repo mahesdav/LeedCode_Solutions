79eb972f57efaec7a326639c517fcd275433502d
mahesdav <35679809+mahesdav@users.noreply.github.com>
2023-10-30 12:25:08 +0530

    Time: 2 ms (23.49%), Space: 55.9 MB (45.2%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,41 +1,73 @@
 class Solution {
     public void rotate(int[] nums, int k) {
 
-        if(nums.length==1){
-            return;
+        // solution with O(1) space complexity (start)
+        // reverse the array first
+        //then, reverse subarray from 0-(k-1) and k-(nums.length-1)
+        if(nums.length<=k)
+          k=k%nums.length;
+        int len = nums.length-1;
+        int i=0;
+        int j=len;
+        // reverse the array
+        while(i<j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+        }
+        //now reverse the subarray from 0 to (k-1)
+        i=0;
+        j=k-1;
+        while(i<j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
         }
 
-        int[] result = new int[nums.length];
-        int j=0;
-        if(k>nums.length){
-    //         int n = nums.length;
-    //         for (int i = 0; i < n / 2; i++) {
-    //     int temp = nums[i];
-    //     nums[i] = nums[n - i - 1];
-    //     nums[n - i - 1] = temp;
-    // }
-    //         return;
-    k = k%nums.length;
+        //now reverse the subarray from k to (nums.length-1)
+        i=k;
+        j=len;
+        while(i<j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
         }
+
+        // solution with O(1) space complexity (end)
+
+
+        // solution with O(n) space complexity (start)
+
+        // if(nums.length==1){
+        //     return;
+        // }
+
+        // int[] result = new int[nums.length];
+        // int j=0;
+        // if(k>nums.length){
+        //     k = k%nums.length;
+        // }
           
-        for(int i=nums.length-k;i<nums.length;i++){
-            result[j]=nums[i];
-               System.out.println("result[j] is : " + result[j]);
-            j++;
-        }
-      //   System.out.println("result.length is : " + result.length);
-        for(int i=0;i<nums.length-k;i++){
-           
-            //  System.out.println("j is : " + j);
-            //   System.out.println("nums[i] is : " + nums[i]);
-               
-            result[j]=nums[i];
-             System.out.println("result[j] is : " + result[j]);
-            j++;
-        }
-        for(int i=0;i<result.length;i++){
-            nums[i]=result[i];
-        }
-       // return result;
+        // for(int i=nums.length-k;i<nums.length;i++){
+        //     result[j]=nums[i];
+        //        System.out.println("result[j] is : " + result[j]);
+        //     j++;
+        // }
+        // for(int i=0;i<nums.length-k;i++){  
+        //     result[j]=nums[i];
+        //      System.out.println("result[j] is : " + result[j]);
+        //     j++;
+        // }
+        // for(int i=0;i<result.length;i++){
+        //     nums[i]=result[i];
+        // }
+
+        //  solution with O(n) space complexity (end)
     }
 }
